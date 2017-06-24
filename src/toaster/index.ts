94302e2a364f94670ee3e0c7102016488895ade7
Yaroslav Admin <devoto13@gmail.com>
2017-06-24 18:18:38 +0200

    Add everything to exports

--- src/toaster/index.ts
+++ src/toaster/index.ts
@@ -1,1 +1,5 @@
 export { ToasterModule } from './toaster.module';
+export { ToasterService } from './toaster.service';
+export { Toast } from './toast';
+export { ToastComponent } from './toast.component';
+export { ToastContainerComponent } from './toast-container.component';
